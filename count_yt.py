ac0b7a1fe0f690ca86853a2f8f9512953ae51175
Dasara Shullani <shullani.dasara@gmail.com>
2020-10-09 22:08:05 +0200

    get playlist urls

--- count_yt.py
+++ count_yt.py
@@ -5,14 +5,29 @@ import logging
 import os
 import youtube_dl
 
+
+def get_url_playlist(playlist_path):
+    ydl_opts = {"quiet":True}
+    res = []
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        json_info = ydl.extract_info(playlist_path, download=False)
+        if 'entries' in json_info:
+            for item in json_info['entries']:
+                res.append(item['webpage_url'])
+    with open('yt_list.txt', 'a+') as f:
+        for item in res:
+            f.write(item + "\n")
+
+
 def get_view_count(video_url):
     ydl_opts = {"quiet":True}
+    res = {'title':'', 'count':-1, 'url':''}
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         json_info = ydl.extract_info(video_url, download=False)
-        if 'view_count' in json_info:
-            return json_info['view_count']
-        else:
-            return -1
+        res['title']= json_info['title']
+        res['count'] = json_info['view_count']
+        res['url'] = json_info['webpage_url']
+    return res
 
 def get_date_time_string():
     obj_date = datetime.now()
@@ -22,6 +37,7 @@ def get_date_time_string():
 
 def get_parser():
     args_parser = argparse.ArgumentParser(description='Radio CICAP YouTube count views.')
+    args_parser.add_argument('--store-playlist', action='store_true', help='Save playlist urls flag. Use --video-url to specify the playlist.')
     g = args_parser.add_mutually_exclusive_group(required=True)
     g.add_argument('--video-url', type=str, help='YouTube video URL.')
     g.add_argument('--file-url-list', type=str, help='TxT file of YouTube video URLs.')
@@ -33,12 +49,16 @@ def main():
     args = parser.parse_args()
 
     if args.video_url is not None:
-        video_url = 'https://www.youtube.com/watch?v=Sn7YrWMKOM4'
-        vc = get_view_count(video_url)
-        if vc > -1:
-            print(video_url)
-            print(get_date_time_string())
-            print(vc)
+        if args.store_playlist:
+            get_url_playlist(args.video_url)
+        else:
+            video_url = 'https://www.youtube.com/watch?v=Sn7YrWMKOM4'
+            vc = get_view_count(video_url)
+            if vc['count'] > -1:
+                note_str = "{}; {}; {}; {}".format(vc['title'], vc['url'],
+                                                   get_date_time_string(),
+                                                   vc['count'])
+                print(note_str)
 
 if __name__ == '__main__':
     main()
